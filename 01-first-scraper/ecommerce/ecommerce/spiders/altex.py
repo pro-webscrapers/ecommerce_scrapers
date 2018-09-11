73303c052f89dfec0a6d3d0fab2bbb96993a5b33
wim86 <wimhermans2@hotmail.com>
2018-09-11 21:50:52 +0300

    initial commit first scraper

---
+++ 01-first-scraper/ecommerce/ecommerce/spiders/altex.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import logging
+
+
+class AltexSpider(scrapy.Spider):
+    name = 'altex'
+    allowed_domains = ['altex.ro']
+    start_urls = ['https://altex.ro/apa-espresso-cafea/cafetiere']
+
+    def parse(self, response):
+        items_xpath = response.xpath('//*[@class="Products-item"]')
+        for item_xpath in items_xpath:
+            title = item_xpath.xpath(
+                './/*[@class="Product-nameHeading"]//@title'
+            ).extract()
+            url = item_xpath.xpath(
+                './/*[@class="Product-nameHeading"]/a/@href'
+            ).extract()
+            reduction = item_xpath.xpath(
+                './/*[@class="Badge-reducere"]/text()'
+            ).extract()
+            new_price = item_xpath.xpath(
+                './/*[@itemprop="price"]/@content'
+            ).extract()
+
+            logging.info("title: {0}, "
+                         "url: {1}, "
+                         "reduction: {2}, "
+                         "new_price: {3}, "
+                         .format(title, url, reduction, new_price))
