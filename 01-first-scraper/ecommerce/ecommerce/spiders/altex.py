8894b5f37e8529642fa7d66271ab8c9cdde53df2
wim86 <wimhermans2@hotmail.com>
2018-11-18 14:25:55 +0200

    updated url, small changes in xpaths and reformatting of logging

--- 01-first-scraper/ecommerce/ecommerce/spiders/altex.py
+++ 01-first-scraper/ecommerce/ecommerce/spiders/altex.py
@@ -6,16 +6,17 @@ import logging
 class AltexSpider(scrapy.Spider):
     name = 'altex'
     allowed_domains = ['altex.ro']
-    start_urls = ['https://altex.ro/apa-espresso-cafea/cafetiere']
+    start_urls = ['https://altex.ro/cafetiere/cpl/']
 
     def parse(self, response):
         items_xpath = response.xpath('//*[@class="Products-item"]')
         for item_xpath in items_xpath:
             title = item_xpath.xpath(
-                './/*[@class="Product-nameHeading"]//@title'
+                './/*[@class="Product-list-right"]//@title'
             ).extract()
             url = item_xpath.xpath(
-                './/*[@class="Product-nameHeading"]/a/@href'
+                './/*[@class="Product-list-right"]'
+                '/*[@class="Product-nameHeading"]/a/@href'
             ).extract()
             reduction = item_xpath.xpath(
                 './/*[@class="Badge-reducere"]/text()'
@@ -24,8 +25,10 @@ class AltexSpider(scrapy.Spider):
                 './/*[@itemprop="price"]/@content'
             ).extract()
 
-            logging.info("title: {0}, "
-                         "url: {1}, "
-                         "reduction: {2}, "
-                         "new_price: {3}, "
-                         .format(title, url, reduction, new_price))
+            logging.info(
+                "title: {0}, "
+                "url: {1}, "
+                "reduction: {2}, "
+                "new_price: {3}, "
+                .format(title, url, reduction, new_price)
+            )
